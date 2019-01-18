aed82abf9730264c78515ea7bca3fe6d181943e7
G1nes <timursalakhov96@gmail.com>
2019-01-18 17:46:49 +0300

    write steps settings

--- src/main/java/steps/BaseSteps.java
+++ src/main/java/steps/BaseSteps.java
@@ -1,4 +1,55 @@
 package steps;
 
-public class BaseSteps {
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.ArrayList;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public abstract class BaseSteps {
+    private static WebDriver driver;
+    private static String url;
+    private static Properties stepsProperties=StepsProperties.getInstance().getProperties();
+
+    public BaseSteps(){
+    }
+    @BeforeClass
+    public static void setUp(){
+        switch (stepsProperties.getProperty("browser")){
+            case "chrome":
+                driver = new ChromeDriver();
+                System.setProperty("webdriver.chrome.driver", stepsProperties.getProperty("webdriver.chrome.driver"));
+            case "firefox":
+                driver = new FirefoxDriver();
+                System.setProperty("webdriver.gecko.driver", stepsProperties.getProperty("webdriver.gecko.driver"));
+                default:
+                    driver = new ChromeDriver();
+                    System.setProperty("webdriver.chrome.driver", stepsProperties.getProperty("webdriver.chrome.driver"));
+        }
+        url = stepsProperties.getProperty("url");
+        driver.get(url);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver .manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+    }
+    @AfterClass
+    public static void endTest(){
+        driver.close();
+    }
+
+    public void fillField(WebElement element, String value){
+        element.clear();
+        element.sendKeys(value);
+    }
+    public void selectNextTab(){
+        ArrayList<String>tabs = new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(tabs.get(tabs.size()-1));
+    }
 }
--- src/main/java/steps/StepsProperties.java
+++ src/main/java/steps/StepsProperties.java
@@ -1,4 +1,26 @@
 package steps;
 
+import java.io.*;
+import java.util.Properties;
+
 public class StepsProperties {
+    private Properties properties = new Properties();
+    private static StepsProperties INSTANCE = null;
+    private StepsProperties() {
+        try {
+            properties.load(new FileInputStream(new File("C:\\Program Files\\Repositories\\InsuranceTest\\InsuranceTest.properties")));
+        }catch (Exception e){
+            e.getStackTrace();
+        }
+
+    }
+    public static StepsProperties getInstance() {
+        if (INSTANCE == null){
+            INSTANCE = new StepsProperties();
+        }
+        return INSTANCE;
+    }
+    public Properties getProperties (){
+        return properties;
+    }
 }
